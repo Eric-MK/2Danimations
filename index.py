3a7924aad02b03da3bbd035845d969f700fb9143
unknown <mutungaeric839@gmail.com>
2023-10-14 16:18:06 +0300

    drawing a rectangle image

--- index.py
+++ index.py
@@ -1,8 +1,15 @@
 import cairo
 
 surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400) #create an image surface (PNG) of size 600, 400
-# Create a Context and Set the Fill Color
+# Create a Context and Set the Fill Color A Cairo context is created to work with the image surface. The context is like a "pen" that you use to draw on the canvas.
 ctx = cairo.Context(surface)
 ctx.set_source_rgb(0.8, 0.8, 0.8)
 # Fill the Entire Surface with the Set Color
 ctx.paint()
+
+# Draw the image
+ctx.rectangle(150, 100, 100, 240)
+# sets the fill color to red.
+ctx.set_source_rgb(1,0,0)
+#  fills the defined rectangle with the red color, effectively drawing a red rectangle on the canvas.
+ctx.fill() 
