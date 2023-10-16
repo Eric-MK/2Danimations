e5f104a04795e7aed78c8ae991c8b58005a93b22
unknown <mutungaeric839@gmail.com>
2023-10-16 05:15:32 +0300

    line joins

---
+++ linejoins.py
@@ -0,0 +1,36 @@
+import cairo
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400) #create an image surface (PNG) of size 600, 400
+# Create a Context and Set the Fill Color A Cairo context is created to work with the image surface. The context is like a "pen" that you use to draw on the canvas.
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()
+
+
+# set line color width
+ctx.set_source_rgb(0,0,0)
+ctx.set_line_width(20)
+
+# Miter join
+ctx.move_to(50, 100)
+ctx.line_to(180, 300)
+ctx.line_to(50, 300)
+ctx.set_line_join(cairo.LINE_JOIN_MITER)
+ctx.stroke()
+
+# Round join
+ctx.move_to(240, 100)
+ctx.line_to(370, 300)
+ctx.line_to(240, 300)
+ctx.set_line_join(cairo.LINE_JOIN_ROUND)
+ctx.stroke()
+
+# Bevel join
+ctx.move_to(430, 100)
+ctx.line_to(560, 300)
+ctx.line_to(430, 300)
+ctx.set_line_join(cairo.LINE_JOIN_BEVEL)
+ctx.stroke()
+
+file_path = "output/linejoins.png"
+
+surface.write_to_png(file_path)
