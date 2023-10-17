2eb481007314b22cb8e769898cb8bb31c1dff415
unknown <mutungaeric839@gmail.com>
2023-10-17 09:11:47 +0300

    bezierpolycurve

--- beziercurve.py
+++ beziercurve.py
@@ -6,14 +6,16 @@ ctx.set_source_rgb(0.8, 0.8, 0.8)
 ctx.paint()
 
 # Bezier curve
-ctx.move_to(100, 200)
-ctx.curve_to(200, 100, 400, 300, 500, 200)
+ctx.move_to(100, 100)
+ctx.curve_to(200, 0, 400, 200, 500, 100)
+ctx.line_to(500, 300)
+ctx.curve_to(400, 400, 200, 200, 100, 300)
+ctx.close_path()
 ctx.set_source_rgb(1,0,0)
+ctx.set_dash([40, 10])
 ctx.set_line_width(10)
 ctx.stroke()
 
-
-
-file_path = "output/Beziercurve.png"
+file_path = "output/polycurvesbazier.png"
 
 surface.write_to_png(file_path)
