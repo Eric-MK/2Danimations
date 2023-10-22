d51ea59346ef0e89f3c1a11883f35913381f5e76
unknown <mutungaeric839@gmail.com>
2023-10-22 13:09:19 +0300

    line with angles

---
+++ lineanglelength.py
@@ -0,0 +1,24 @@
+import cairo
+import math
+
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400) #create an image surface (PNG) of size 600, 400
+# Create a Context and Set the Fill Color A Cairo context is created to work with the image surface. The context is like a "pen" that you use to draw on the canvas.
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()
+
+ctx.set_source_rgb(0,0,0)
+ctx.set_line_width(10)
+
+# relative drawing function
+a = 0.523599 # radian equals 30 degree
+r = 200
+x1 = 300
+y1 = 200
+ctx.move_to(x1,y1)
+ctx.line_to(x1 + r*math.cos(a), y1 + r*math.sin(a))
+ctx.stroke()
+
+file_path = "output/lineangle.png"
+
+surface.write_to_png(file_path)
